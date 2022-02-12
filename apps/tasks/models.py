0c951f67ebf760bd1c19c70ab1cd3c88f2e74478
[artemkka] <[artemkka2280@gmail.com]>
2022-02-12 21:36:53 +0200

    added model task

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return f'{self.title}'
