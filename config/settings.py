f6c4f79a92144217f7608aaa23f1064e9290fa94
Artem Pryatka <artem.pryatka@internal.ebs.md>
2022-03-07 13:51:23 +0200

    changed password

--- config/settings.py
+++ config/settings.py
@@ -172,6 +172,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'artemkka2280@gmail.com'
-EMAIL_HOST_PASSWORD = 'redmi62006'
+EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
