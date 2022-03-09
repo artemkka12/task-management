96d940ebc187aea0a8cba5e720b15fc34c4ce952
Artem Pryatka <artem.pryatka@internal.ebs.md>
2022-03-09 15:36:49 +0200

    refactoring

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,9 @@ from helpers import schema_view
 urlpatterns = [
                   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
                   path('admin/', admin.site.urls),
-                  path('', include('apps.users.urls')),
+                  path('user/', include('apps.users.urls')),
                   path('', include('apps.tasks.urls')),
-                  path('', include('apps.comments.urls')),
+                  path('comment/', include('apps.comments.urls')),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
