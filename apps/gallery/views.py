196006b2811cbd2d7a5d953ad572fc2f799ae8ce
Artem Pryatka <artem.pryatka@internal.ebs.md>
2022-03-09 17:58:50 +0200

    refactoring

--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -1,4 +1,3 @@
-from minio import Minio
 from rest_framework import parsers, renderers
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,18 +10,3 @@ class PictureViewSet(ModelViewSet):
     renderer_classes = (renderers.JSONRenderer,)
     queryset = Picture.objects.all()
     serializer_class = PictureSerializer
-
-    # def post(self, request):
-    #     serializer = self.serializer_class(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     validated_data = serializer.validated_data
-    #
-    #     client = Minio(
-    #         "10.1.1.174:9000",
-    #         access_key="H0T3P3FNGDTFFSR7353I",
-    #         secret_key="lF7Vc8jiRntTaIVlYAX+3+5PUey+5xmrcvpegtzd",
-    #         secure=False
-    #     )
-    #
-    #
-    #
